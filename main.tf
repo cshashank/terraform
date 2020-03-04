afa646586135e919700b49a85d9489e9c5709016
cshashank <shashank@timbergrove.com>
2020-03-04 01:41:23 -0500

    created aws security instance group

--- main.tf
+++ main.tf
@@ -11,3 +11,16 @@ resource "aws_s3_bucket" "b" {
       Environment = "Dev"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name= "terraform-security-instance"
+
+  ingress{
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
